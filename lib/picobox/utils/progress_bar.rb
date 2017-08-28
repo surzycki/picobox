d98243b105108a699d219ad37ffac7235aff608b
Stefan <stefan.surzycki@gmail.com>
2017-08-28 15:19:34 +0200

    moved module to project standard

--- lib/picobox/utils/progress_bar.rb
+++ lib/picobox/utils/progress_bar.rb
@@ -1,29 +1,31 @@
-module Picobox::Utils
-  class ProgressBar
-    class << self
-      def new(size)
-        @progress_bar = ::ProgressBar.create(
-          format:         "%b\u{15E7}%i %p%% %t",
-          progress_mark:  ' ',
-          remainder_mark: "\u{FF65}",
-          total:          size
-        )
+module Picobox
+  module Utils
+    class ProgressBar
+      class << self
+        def new(size)
+          @progress_bar = ::ProgressBar.create(
+            format:         "%b\u{15E7}%i %p%% %t",
+            progress_mark:  ' ',
+            remainder_mark: "\u{FF65}",
+            total:          size
+          )
+        end
+
+        def step(step)
+          @progress_bar.progress = step
+        end
+
+
+        def increment
+          @progress_bar.progress += 1
+        end
+
+        def reset()
+        end
+
+        private
+        attr_accessor :progress_bar
       end
-
-      def step(step)
-        @progress_bar.progress = step
-      end
-
-
-      def increment
-        @progress_bar.progress += 1
-      end
-
-      def reset()
-      end
-
-      private
-      attr_accessor :progress_bar
     end
   end
 end
