ab425df894aa126674d2ce5635debedc9aa0aaea
Stefan <stefan.surzycki@gmail.com>
2017-09-03 01:31:23 +0200

    debugging

--- lib/picobox/commands/stop.rb
+++ lib/picobox/commands/stop.rb
@@ -8,8 +8,9 @@ module Picobox
 
         raise Errors::ProjectNotInitialized unless project_initialized?
 
-        status = system('docker-compose down 2>/dev/null') if project_running?
-        puts status
+        #system('docker-compose down 2>/dev/null') if project_running?
+        system('docker-compose down') if project_running?
+
 
         publish_event :stopped
       end
