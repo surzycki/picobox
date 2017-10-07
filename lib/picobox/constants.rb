9b4b8045e9ea93e6d601c69dcfe7b20c30b3a287
Stefan <stefan.surzycki@gmail.com>
2017-10-07 21:00:48 +0200

    update version

--- lib/picobox/constants.rb
+++ lib/picobox/constants.rb
@@ -1,6 +1,6 @@
 module Picobox
   # names for files / directories
-  VERSION           = '0.3.2'
+  VERSION           = '0.3.3'
   HOMEPAGE          = 'https://github.com/surzycki/picobox'
   PACKAGES_INFO_URL = 'https://api.github.com/repos/picobox/packages/releases/latest'
   CONFIG_DIR        = '.picobox'
